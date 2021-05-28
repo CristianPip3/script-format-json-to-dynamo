f823de40e1685573ac3c48de0f7ea73ab5fc805c
CristianPip3 <cristianbenavides@unicauca.edu.co>
2021-05-28 12:46:09 -0500

    Refactor code runScript

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 const fs = require('fs')
 const attr = require('dynamodb-data-types').AttributeValue;
+
 const formatterJson = async() => {
   const dataString = await fs.promises.readFile('co_inspira_co-P8.json', 'utf8' )
   const data = JSON.parse(dataString)
   console.log(data.length)
-    const items = data.map((item) =>{
-      const newKey = 'percent';
-      const keyOldEmpty = '';
-      const keyId = 'id';
-      const oldKey = '%';
-      delete Object.assign(item, {[newKey]: item[oldKey] })[oldKey];
-      delete Object.assign(item, {[keyId]: item[keyOldEmpty] })[keyOldEmpty];
-      return item
-    })
-
-    let dataWrite = JSON.stringify(items);
-    await fs.promises.writeFile('data-clean.json', dataWrite);
+  const items = data.map((item) =>{
+    const newKey = 'percent';
+    const keyOldEmpty = '';
+    const keyId = 'id';
+    const oldKey = '%';
+    delete Object.assign(item, {[newKey]: item[oldKey] })[oldKey];
+    delete Object.assign(item, {[keyId]: item[keyOldEmpty] })[keyOldEmpty];
+    return item
+  })
+  await formatterJsonToDynamoDb(items)
 }
-const formatterJsonToDynamoDb = async () =>{
-  const dataLoaded = await fs.promises.readFile('data-clean.json', 'utf8')
-  const data = JSON.parse(dataLoaded)
-  const items = data.map((element) =>{
+
+const formatterJsonToDynamoDb = async (data) =>{
+
+  const items = data.map((element) => {
+
     if(Array.isArray(element.PROYECTOS)){
 
-      element.PROYECTOS = element.PROYECTOS.map((proyecto, index) => ({[index]:proyecto}))
+      element.PROYECTOS = element.PROYECTOS.map((project, index) => ({[index]:project}))
 
-    }else{
+    }else {
       element.PROYECTOS = [];
     }
     const itemDynamo = attr.wrap(element)
-    return { PutRequest:
-          {
-            Item: itemDynamo
-          }
-    }
+    return { PutRequest: { Item: itemDynamo } }
   })
+
   console.log(items.length)
-  const newData={
+
+  const newData = {
     coInspira: items
   }
-  let dataWrite = JSON.stringify(newData);
+  const dataWrite = JSON.stringify(newData);
   await fs.promises.writeFile('data-dynamo.json', dataWrite);
 }
 const runScript = async () =>{
   await formatterJson()
-  await formatterJsonToDynamoDb()
 }
 
 runScript();
